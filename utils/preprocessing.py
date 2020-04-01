bf90d59245eb2e971d539b1c82c8e68d54792ba9
Zhao Qi <qi.zhao1991@gmail.com>
2020-04-01 22:34:11 +0200

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -13,8 +13,12 @@ import cv2
 import tools
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
-
+def test(image):
+    plt.figure()
+    plt.imshow(image)
+    plt.show()
 
 def resize_image(img_in, img_shape, resize_mode = cv2.INTER_LINEAR):
     """resize input image
@@ -22,16 +26,16 @@ def resize_image(img_in, img_shape, resize_mode = cv2.INTER_LINEAR):
 
     Parameters
     ----------
-    img_in : 3D array of shape [h, w, c] 
-        input image.
-    img_shape : tuple or list 
-        DESCRIPTION.
-    resize_mode : TYPE, optional
-        DESCRIPTION. The default is cv2.INTER_CUBIC.
+        img_in : 3D array of shape [h, w, c] 
+            input image.
+        img_shape : tuple or list 
+            DESCRIPTION.
+        resize_mode : string, optional
+            Interpolation algorithm. The default is cv2.INTER_CUBIC.
 
     Returns
     -------
-    None.
+        Resized image.
 
     """
     if not isinstance(img_in, np.ndarray):
@@ -43,6 +47,33 @@ def resize_image(img_in, img_shape, resize_mode = cv2.INTER_LINEAR):
     resized_img = cv2.resize(img_in, (img_shape[0], img_shape[1]), interpolation=resize_mode)
     
     return resized_img
+
+def crop_image(img_in, rect):
+    """
+    
+
+    Parameters
+    ----------
+    img_in : TYPE
+        DESCRIPTION.
+    rect : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    img = img_in.copy()
+    x = rect[0]
+    y = rect[1]
+    w = rect[2]
+    h = rect[3]
+    x_1 = x + w
+    y_1 = y + h
+    return img[y:y_1, x:x_1, :], [x, y, x_1, y_1, w, h]
+    
 
 
 
