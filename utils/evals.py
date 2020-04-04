735a58ffbd2186ab65c30e18fea46b40fce7dd51
Zhao Qi <qi.zhao1991@gmail.com>
2020-04-04 23:28:51 +0200

    Create evals.py

---
+++ utils/evals.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  4 23:25:25 2020
+
+@author: qizhao
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+
+
+def if_intersection():
+    
+    
+    return 0
